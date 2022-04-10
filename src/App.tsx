0a89ce4444a24228ee9e49ef65741a565796bedf
shuaga <shuaga@microsoft.com>
2022-04-10 22:30:48 +0530

    making it more friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,60 @@
+import { DefaultButton, PrimaryButton } from '@fluentui/react';
 import React, { useEffect, useState } from 'react';
-import { setOriginalNode } from 'typescript';
 import './App.css';
 
 function App() {
-  const [yesClicked, setYesClicked] = useState(false);
-  const [noClickCount, setNoClickCount] = useState(0);
+    const [yesClicked, setYesClicked] = useState(false);
+    const [noClickCount, setNoClickCount] = useState(0);
+    const [question, setQuestion] = useState('Are you dumb?');
 
-  useEffect(() => {
-    if(noClickCount > 0) {
-      const button = document.getElementById("noButton")!;
-      button.style.position = 'absolute';
-      button.style.top = Math.random()*window.innerHeight + 'px';
-      button.style.left = Math.random()*window.innerWidth + 'px';
-    }
-  }, [noClickCount]);
+    useEffect(() => {
+        if (noClickCount > 0) {
+            const button = document.getElementById("noButton")!;
+            button.style.position = 'absolute';
+            button.style.top = (Math.random() * window.innerHeight/2 + window.innerHeight/3) + 'px';
+            button.style.left = (Math.random() * window.innerWidth/2 + window.innerWidth/3) + 'px';
+        }
+        if(noClickCount > 5) {
+            setQuestion('Are you dumb? Hint: Click yes!');
+        }
+    }, [noClickCount]);
 
-  return (
-    <div className="Container">
-      <div className="App">
-        {!yesClicked && <><h1>Are you dumb?</h1>
-          <YesButton setYesClicked={setYesClicked} />
-          <NoButton setNoClickCount={setNoClickCount} />
-        </>}
-        {yesClicked && <h1>I knew it! B) </h1>}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            {!yesClicked && <><div className='question'>{question}</div>
+            
+            <div className="answer-space">
+                <YesButton setYesClicked={setYesClicked} />
+                <NoButton setNoClickCount={setNoClickCount} />
+            </div></>}
+            {yesClicked && 
+                <div className='question'>
+                    <div>I knew it! </div>
+                    <div>😝 😝 😝</div>
+                    <div>🤣 🤣 🤣</div>
+                    <div>😂 😂 😂</div>
+                    <div>😎 😎 😎</div>
+                    <div>Just kidding!</div>
+                    <div>(Or am I?)</div>
+                </div>}
+        </div>
+    );
 }
 
 const YesButton = (props: { setYesClicked: any }) => {
-  return <button className='Button Yes' onClick={() => {
-    props.setYesClicked(true);
-  }}>Yes</button>
+    return (
+        <div className='button yes'>
+            <PrimaryButton onClick={() => { props.setYesClicked(true); }}>Yes</PrimaryButton>
+        </div>
+    );
 }
 
 const NoButton = (props: { setNoClickCount: any }) => {
-  return <button className='Button No' id="noButton" onClick={() => {
-    props.setNoClickCount((count: number) => count + 1);
-  }}>No</button>
+    return (
+        <div className='button no'>
+            <DefaultButton id="noButton" onClick={() => { props.setNoClickCount((count: number) => count + 1); }}>No</DefaultButton>
+        </div>
+    );
 }
 
 
